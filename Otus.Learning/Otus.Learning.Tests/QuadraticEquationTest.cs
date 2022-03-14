ca2d7a4419630cb809d8823377a29e386196fe99
Fedor Andriyaninkov <fandriyaninkov@gmail.com>
2022-03-15 00:30:16 +0300

    Реализация метода для решения квадратного уравнения + тесты

---
+++ Otus.Learning/Otus.Learning.Tests/QuadraticEquationTest.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Xunit;
+
+namespace Otus.Learning.Tests;
+
+public class QuadraticEquationTest
+{
+    /// <summary>
+    /// Тест проверяет, что для уравнения x^2 + 1 = 0 - корней нет
+    /// </summary>
+    [Fact]
+    public void QuadraticEquationSolve_NoRoots_Test()
+    {
+        // Arrange
+        var a = 1;
+        var b = 0;
+        var c = 1;
+        var quadratic = new QuadraticEquation();
+        
+        // Act
+        var result = quadratic.Solve(a, b, c);
+        
+        // Assert
+        Assert.Empty(result);
+    }
+
+    /// <summary>
+    /// Тест проверяет, что для уравнения x^2 - 1 = 0, есть два корня(1, -1)
+    /// </summary>
+    [Fact]
+    public void QuadraticEquationSolve_TwoRoots_Test()
+    {
+        // Arrange
+        var a = 1;
+        var b = 0;
+        var c = -1;
+        var quadratic = new QuadraticEquation();
+        
+        // Act
+        var result = quadratic.Solve(a, b, c);
+        
+        // Assert
+        Assert.NotEmpty(result);
+        Assert.Equal(2, result.Length);
+        Assert.Equal(1, result[0]);
+        Assert.Equal(-1, result[1]);
+    }
+
+    /// <summary>
+    /// Тест проверяет, что уравнение вида x^2 + 2x + 1 = 0, имеет один корень (-1)
+    /// </summary>
+    [Fact]
+    public void QuadraticEquationSolve_SingleRoot_Test()
+    {
+        // Arrange
+        var a = 1;
+        var b = 2;
+        var c = 1;
+        var quadratic = new QuadraticEquation();
+        
+        // Act
+        var result = quadratic.Solve(a, b, c);
+        
+        // Assert
+        Assert.NotEmpty(result);
+        Assert.Single(result);
+        Assert.Equal(-1, result[0]);
+    }
+
+    /// <summary>
+    /// Тест, который проверяет, что коэффициент 'a' не может быть равен нулю. Результат: Исключение
+    /// </summary>
+    [Fact]
+    public void QuadraticEquationSolve_ThrowExceptionIfAZero_Test()
+    {
+        // Arrange
+        var a = 0;
+        var b = 2;
+        var c = 1;
+        var quadratic = new QuadraticEquation();
+        
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => quadratic.Solve(a, b, c));
+    }
+
+    /// <summary>
+    /// Тест, который проверяет, что дескриминант находится в диапазоне от +epsilon до -epsilon
+    /// </summary>
+    [Fact]
+    public void QuadraticEquationSolve_CompareCoefficientsLessEpsilon_Test()
+    {
+        // Arrange
+        var a = 1;
+        var b = 0.025;
+        var c = 0.0001;
+        var quadratic = new QuadraticEquation();
+        
+        
+        //Act
+        var result = quadratic.Solve(a, b, c);
+        
+        // Assert
+        Assert.NotEmpty(result);
+        Assert.Single(result);
+        Assert.Equal(-0.0125, Math.Round(result[0], 5));
+    }
+    
+    /// <summary>
+    /// Тест, который проверяет некорректность коэффициентов
+    /// </summary>
+    [Fact]
+    public void QuadraticEquationSolve_CoefficientsNan_Test()
+    {
+        // Arrange
+        var a = double.NaN;
+        var b = 3;
+        var c = 2;
+        var quadratic = new QuadraticEquation();
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => quadratic.Solve(a, b, c));
+    }
+    
+    /// <summary>
+    /// Тест, который проверяет некорректность коэффициентов
+    /// </summary>
+    [Fact]
+    public void QuadraticEquationSolve_CoefficientsNegativeInfinity_Test()
+    {
+        // Arrange
+        var a = 1;
+        var b = Double.NegativeInfinity;
+        var c = 2;
+        var quadratic = new QuadraticEquation();
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => quadratic.Solve(a, b, c));
+    }
+    
+    /// <summary>
+    /// Тест, который проверяет некорректность коэффициентов
+    /// </summary>
+    [Fact]
+    public void QuadraticEquationSolve_CoefficientsPositiveInfinity_Test()
+    {
+        // Arrange
+        var a = 1;
+        var b = 3;
+        var c = Double.PositiveInfinity;
+        var quadratic = new QuadraticEquation();
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => quadratic.Solve(a, b, c));
+    }
+    
+    /// <summary>
+    /// Тест проверяет, является ли дискриминант вещественным числом
+    /// </summary>
+    [Fact]
+    public void QuadraticEquationSolve_CoefficientsMaxValue_Test()
+    {
+        // Arrange
+        var a = Double.MaxValue;
+        var b = 3;
+        var c = 5;
+        var quadratic = new QuadraticEquation();
+
+        // Act & Assert
+        Assert.Throws<ArithmeticException>(() => quadratic.Solve(a, b, c));
+    }
+}
---
+++ Otus.Learning/Otus.Learning/QuadraticEquation.cs
@@ -0,0 +1,86 @@
+﻿namespace Otus.Learning;
+
+public class QuadraticEquation
+{
+    private readonly double _epsilon = 0.001;
+
+    public QuadraticEquation()
+    {
+        
+    }
+    public QuadraticEquation(double epsilon)
+    {
+        _epsilon = epsilon;
+    }
+
+    /// <summary>
+    /// Метод находит корни квадратного уравнения вида: a*x2 + b*x + c = 0
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="c"></param>
+    /// <returns>Массив корней уравнения</returns>
+    public double[] Solve(double a, double b, double c)
+    {
+        IfCoefficientsNotFiniteThenException(a, b, c);
+        IfCoefficientANotEpsilonRangeThenException(a);
+
+        var discriminant = Math.Pow(b, 2) - 4 * a * c;
+        IfDiscriminantNotFiniteThenException(discriminant);
+        if (Math.Abs(discriminant) < _epsilon)
+        {
+            var x = -b / (2 * a);
+            return new[] { x };
+        }
+
+        if (discriminant > _epsilon)
+        {
+            var x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            var x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+            return new[] { x1, x2 };
+        }
+        
+        return Array.Empty<double>();
+    }
+
+    /// <summary>
+    /// Метод проверяет, являются ли коэффициенты уравнения вещественными числами 
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="c"></param>
+    /// <exception cref="ArgumentException"></exception>
+    private void IfCoefficientsNotFiniteThenException(double a, double b, double c)
+    {
+        if (!double.IsFinite(a) || !double.IsFinite(b) || !double.IsFinite(c))
+        {
+            throw new ArgumentException("Коэффициенты уравнения должны быть вещестенным числом");
+        }
+    }
+
+    /// <summary>
+    /// Метод проверяет находится ли коэффициента 'а' в диапазоне точности
+    /// </summary>
+    /// <param name="a"></param>
+    /// <exception cref="ArgumentException"></exception>
+    private void IfCoefficientANotEpsilonRangeThenException(double a)
+    {
+        if (Math.Abs(a) < _epsilon)
+        {
+            throw new ArgumentException("В квадратном уравнение коэффициент 'a' не может быть равен 0");
+        }
+    }
+
+    /// <summary>
+    /// Метод проверяет, является ли дискриминант вещественным числом
+    /// </summary>
+    /// <param name="d"></param>
+    /// <exception cref="ArithmeticException"></exception>
+    private void IfDiscriminantNotFiniteThenException(double d)
+    {
+        if (!double.IsFinite(d))
+        {
+            throw new ArithmeticException("Дискриминант должен быть вещественным числом");
+        }
+    }
+}
